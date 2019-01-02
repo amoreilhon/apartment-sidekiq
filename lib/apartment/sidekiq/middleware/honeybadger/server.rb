97f65e0fbf2e9fe5127237f94d92728cbc171c80
Alexandre MOREILHON <alexandremoreilhon@MacBook-Pro-de-Alexandre.local>
2019-01-02 14:38:37 +0100

    fork for test

--- lib/apartment/sidekiq/middleware/honeybadger/server.rb
+++ lib/apartment/sidekiq/middleware/honeybadger/server.rb
@@ -1,7 +1,7 @@
 module Apartment::Sidekiq::Middleware::Honeybadger
   class Server
     def call(worker_class, item, queue)
-      Honeybadger.context({tenant: Apartment::Tenant.current})
+      Honeybadger.context({tenant: Tenant.current})
 
       yield
     end
